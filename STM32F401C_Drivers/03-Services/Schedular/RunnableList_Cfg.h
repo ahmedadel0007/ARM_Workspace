334fbc7152dec6221a41af67df97a0d7ca4a646f
Ahmed Adel <ahmed.adel5abdalla@gmail.com>
2024-03-12 19:04:21 -0700

    Upload__Schedular

---
+++ STM32F401C_Drivers/03-Services/Schedular/RunnableList_Cfg.h
@@ -0,0 +1,23 @@
+/*
+ * RunnableList_Cfg.h
+ *
+ *  Created on: Mar 12, 2024
+ *      Author: HP
+ */
+
+#ifndef RUNNABLELIST_CFG_H_
+#define RUNNABLELIST_CFG_H_
+
+
+typedef enum{
+	app1,
+	app2,
+	_RUN_NUM
+}Run_t;
+
+
+
+extern void Runnable_Toggle_Led1(void) ;
+extern void Runnable_Toggle_Led2(void) ;
+
+#endif /* RUNNABLELIST_CFG_H_ */
